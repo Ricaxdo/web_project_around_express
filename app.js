9c93450aa7c9763f0b25aab84eef9a481b9b8cf2
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-11-29 05:41:22 -0600

    feat: conexion a mongodb + autorizacion temporal

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose'); // <-- Mongoose
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const app = express();
 const PORT = 3000;
+
+// Conexión a MongoDB
+mongoose.connect('mongodb://localhost:27017/aroundb')
+  .then(() => {
+    console.log('Conectado a MongoDB: aroundb');
+  })
+  .catch((err) => {
+    console.error('Error de conexión a MongoDB:', err);
+  });
+
+const db = mongoose.connection;
+
+// Middleware temporal de autorización
+app.use((req, res, next) => {
+  req.user = {
+    _id: '692ad209b630c2ff7caa5350',
+  };
+  next();
+});
+
+// Eventos de conexión
+db.on('error', (err) => {
+  console.error('Error de conexión a MongoDB:', err);
+});
+
+db.once('open', () => {
+  console.log('Conectado a MongoDB: aroundb');
+});
 
 // Middleware para parsear JSON
 app.use(express.json());
@@ -17,7 +46,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Recurso solicitado no encontrado' });
 });
 
-// Inicia el servidor y escucha en el puerto 3000
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
